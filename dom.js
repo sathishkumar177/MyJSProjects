20547076aea3ed30604dc456240a03f7a57935e9
sathishkumar177 <sathishkumarblessed@gmail.com>
2022-11-04 08:57:28 +0530

    version 11.4.2

--- dom.js
+++ dom.js
@@ -1,2 +1,19 @@
-console.log(document.domain);
-console.dir(document);
+// console.log(document.domain);
+// console.dir(document);
+
+document.title='New title';
+console.log(document.title);
+var header=document.getElementById('header-title');
+header.innerText='Hello      All';
+//header.textContent='Hello       All';
+console.log(header.innerText);
+// console.log();
+
+
+var headerTitle = document.getElementById('main-header');
+headerTitle.style.borderBottom = 'solid 3px #000';
+
+
+var addItem = document.querySelector('.title');
+addItem.style.color = 'green';
+addItem.fontweight = 'bold';

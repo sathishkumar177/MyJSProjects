e864dbed54d0944ef2bd2b383b77e5a63099064e
sathishkumar177 <sathishkumarblessed@gmail.com>
2022-11-07 10:41:05 +0530

    November 7th

--- dom.js
+++ dom.js
@@ -1,19 +1,49 @@
-// console.log(document.domain);
-// console.dir(document);
+var itemList = document.querySelector('#items');
+//parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor='yellow';
+// console.log(itemList.parentNode.parentNode);
+// itemList.parentNode.parentNode.style.backgroundColor='black';
 
-document.title='New title';
-console.log(document.title);
-var header=document.getElementById('header-title');
-header.innerText='Hello      All';
-//header.textContent='Hello       All';
-console.log(header.innerText);
-// console.log();
-
-
-var headerTitle = document.getElementById('main-header');
-headerTitle.style.borderBottom = 'solid 3px #000';
+// //parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor='yellow';
+// console.log(itemList.parentElement.parentNode);
+// itemList.parentElement.parentElement.style.backgroundColor='black';
 
 
-var addItem = document.querySelector('.title');
-addItem.style.color = 'green';
-addItem.fontweight = 'bold';
+//childNodes
+// console.log(itemList.childNodes);
+
+// console.log(itemList.children);
+// itemList.children[1].style.backgroundColor='Yellow';
+
+// itemList.firstElementChild.style.backgroundColor='pink';
+
+// itemList.lastElementChild.style.backgroundColor='violet';
+
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.backgroundColor='Yellow';
+
+//createElement
+
+var newDiv=document.createElement('div');
+newDiv.className='Hello';
+newDiv.id='Hello-1';
+newDiv.setAttribute('title','Hellow Div');
+
+var newDivText = document.createTextNode('Hello World');
+newDiv.appendChild(newDivText);
+console.log(newDiv);
+
+
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+container.insertBefore(newDiv, h1);
+
+
+
+
+
+
+
